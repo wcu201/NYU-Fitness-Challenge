ce3e0d66ffd0a31051adf9bce7bb37c6b7016748
Brienne Renfurm <brierenfurm@172-16-44-96.dynapool.nyu.edu>
2018-04-16 21:21:00 -0400

    Initial Commit

---
+++ NYU Fitness Challenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NYU Fitness Challenge
+//
+//  Created by Brienne Renfurm on 4/16/18.
+//  Copyright © 2018 William Uchegbu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
